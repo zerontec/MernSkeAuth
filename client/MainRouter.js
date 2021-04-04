259121075b823acf9f602742c5a71027adf59651
zerontec <zerontecnologia1@gmail.com>
2021-04-04 17:57:37 -0400

    Configuariocn de modulos usuaios CRUD- auth Metodos

---
+++ client/MainRouter.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from './core/Home'
+import Users from './user/Users'
+import Signup from './user/Signup'
+import Signin from './auth/Signin'
+import Profile from './user/Profile'
+import EditProfile from './user/EditPofile'
+import PrivateRoute from './auth/PrivateRoute'
+import Menu from './core/Menu'
+
+
+
+
+const MainRouter = () => {
+
+
+return( <div>
+<Menu/>
+<Switch>
+<Route exact path="/" component={Home}/>
+
+<Route path="/users" component={Users}/>
+<Route path="/signup" component={Signup}/>
+<Route path="/signin" component={Signin}/>
+<PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+<Route path= "/user/:userId" componet= {Profile}/>
+
+</Switch>
+
+</div>)
+
+}
+
+export default MainRouter
--- template.js
+++ template.js
@@ -1,12 +1,26 @@
-export default () => {
+export default ({markup, css}) => {
  return `<!doctype html>
  <html lang="en">
  <head>
  <meta charset="utf-8">
+ <link rel="stylesheet"
+href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+rel="stylesheet">
+
  <title>MERN Skeleton</title>
+  <style>
+              a{
+                text-decoration: none;
+                color: #061d95
+              }
+          </style>
  </head>
- <body>
- <div id="root">Hello World</div>
+<body style="margin:0">
+          <div id="root">${markup}</div>
+          <style id="jss-server-side">${css}</style>
+          <script type="text/javascript" src="/dist/bundle.js"></script>
+
  </body>
  </html>`
-}
+}  
